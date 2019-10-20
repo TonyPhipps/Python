74b76c32efc1547e545e53c8bd7367fd96d5f30a
python <anthony.james.phipp@gmail.com>
2019-10-20 13:19:47 -0700

    updates

--- get_processes.py
+++ get_processes.py
@@ -1,8 +1,16 @@
 import os
+import csv
 from process import Process
 
 pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
+processes = [Process(pid).__dict__ for pid in pids]
 
-for pid in pids:
-    this_process = Process(pid)
-    print(this_process)
+keys = processes[0].keys()
+
+with open('processes.csv', 'w') as output_csv:
+    dw = csv.DictWriter(output_csv, keys)
+    dw.writeheader()
+    dw.writerows(processes)
+
+# for process in processes:
+#     print(process)
--- process.py
+++ process.py
@@ -15,24 +15,27 @@ class Process:
         self.host = socket.getfqdn()
         self.date_scanned = datetime.datetime.now().replace(microsecond=0).isoformat()
         self.pid = int(pid)
-        self.status = (open(os.path.join('/proc', pid, 'status'), 'rb')).read().decode()
-        self.cmdline = open(os.path.join('/proc', pid, 'cmdline'), 'rb').read().decode()
+        self.status = (open(os.path.join('/proc', pid, 'status'), 'r')).read()
+        self.cmdline = (open(os.path.join('/proc', pid, 'cmdline'), 'r')).read()
 
         # /proc/pid/status
         # Reference: http://man7.org/linux/man-pages/man5/proc.5.html
-        self.name = re.search(r'Name:\s(.+)', self.status).groups()[0],
-        self.state = re.search(r'State:\s(.+)', self.status).groups()[0],
-        self.ppid = re.search(r'PPid:\s(.+)', self.status).groups()[0],
-        self.real_uid = re.search(r'Uid:\s(.+)', self.status).groups()[0].split()[0],
-        self.effective_uid = re.search(r'Uid:\s(.+)', self.status).groups()[0].split()[1],
-        self.saved_uid = re.search(r'Uid:\s(.+)', self.status).groups()[0].split()[2],
-        self.real_gid = re.search(r'Gid:\s(.+)', self.status).groups()[0].split()[0],
-        self.effective_gid = re.search(r'Gid:\s(.+)', self.status).groups()[0].split()[1],
-        self.saved_gid = re.search(r'Gid:\s(.+)', self.status).groups()[0].split()[2],
-        self.threads = re.search(r'Threads:\s(.+)', self.status).groups()[0],
+        self.name = re.search(r'Name:\s(.+)', self.status).group(1)
+        self.state = re.search(r'State:\s(.+)', self.status).group(1)
+        self.ppid = re.search(r'PPid:\s(.+)', self.status).group(1)
+        self.real_uid = re.search(r'Uid:\s(.+)', self.status).group(1).split()[0]
+        self.effective_uid = re.search(r'Uid:\s(.+)', self.status).group(1).split()[1]
+        self.saved_uid = re.search(r'Uid:\s(.+)', self.status).group(1).split()[2]
+        self.real_gid = re.search(r'Gid:\s(.+)', self.status).group(1).split()[0]
+        self.effective_gid = re.search(r'Gid:\s(.+)', self.status).group(1).split()[1]
+        self.saved_gid = re.search(r'Gid:\s(.+)', self.status).group(1).split()[2]
+        self.threads = re.search(r'Threads:\s(.+)', self.status).group(1)
 
         #/proc/pid/cmdline
-        self.cmdline = self.cmdline.split('\0')[0]
+        self.command = self.cmdline.split('\0')[0]
+
+        del self.status
+        del self.cmdline
 
     def __str__(self):
         return str(self.__dict__)
