6e32d71d521ca7aa1c15eb2316b93297273a25aa
Tony Phipps <tphipps@ciphertechs.com>
2019-12-16 14:19:39 -0700

    updates

--- swimlane_to_pandas.py
+++ swimlane_to_pandas.py
@@ -1,21 +1,19 @@
-from pandas.io.json import json_normalize
 from swimlane import Swimlane
+import pandas
+from io import StringIO
 
 swimlane_url = ''
 user=''
 password=''
 app_id=''
+report_id=''
 
 swimlane = Swimlane(swimlane_url, user, password, verify_ssl=False)
-application = swimlane.apps.get(id=app_id)
 
-report_data = application.reports.build('new_report', limit=0)
-report_data_json = []
+report = swimlane.request('get', '/reports/{}'.format(report_id)).json()
+response = swimlane.request('post', 'search/export', json=report)
+csv = StringIO(response.text)
 
-for record in report_data:
-    json_data = record.for_json()
-    report_data_json.append(json_data)
+panda_data = pandas.read_csv(csv)
 
-json_data_normalized = json_normalize(report_data_json)
-
-print(json_data_normalized)
+print(panda_data)
