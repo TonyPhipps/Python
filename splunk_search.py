930ad0465e60a2a2a368fe476fbb49244bf1b778
Tony Phipps <tphipps@ciphertechs.com>
2020-05-25 13:55:27 -0600

    update

--- splunk_search.py
+++ splunk_search.py
@@ -1,49 +1,84 @@
-import urllib.request, urllib.parse, urllib.error
+# Filename: splunk_search.py
+# Anthony Phipps
+# Copyright: GPLv3
+
+import argparse
+import datetime
 import httplib2
+import json
+import time
+import urllib.request, urllib.parse, urllib.error
 from xml.dom import minidom
 
-baseurl = ''
-userName = ''
-password = ''
+def main():
 
-# Set up search query
-searchQuery = 'index=notable earliest=-5m@s'
-searchQuery = searchQuery.strip()
-# If the query doesn't already start with the 'search' operator or another
-# generating command (e.g. "| inputcsv"), then prepend "search " to it.
-if not (searchQuery.startswith('search') or searchQuery.startswith("|")):
-    searchQuery = 'search ' + searchQuery
-print(searchQuery)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', help='Provide the base URL, usually ending in :8089')
+    parser.add_argument('username', help='Username')
+    parser.add_argument('password', help='Password')
+    parser.add_argument('search', help='Search string')
+    args = parser.parse_args()
 
-# Authenticate with server.
-# Disable SSL cert validation. Splunk certs are self-signed.
-serverContent = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/auth/login',
-    'POST', headers={}, body=urllib.parse.urlencode({'username':userName, 'password':password}))[1]
-#print(serverContent)
-sessionKey = minidom.parseString(serverContent).getElementsByTagName('sessionKey')[0].childNodes[0].nodeValue
-print(sessionKey)
+    now = datetime.datetime.now()
+    nowFileName = now.strftime('%Y%m%d-%H%M%S')
 
-# Run the search and get the search ID.
-# Disable SSL cert validation. Splunk certs are self-signed.
-searchResponse = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/search/jobs','POST',
-    headers={'Authorization': 'Splunk {}'.format(sessionKey)}, body=urllib.parse.urlencode({'search': searchQuery}))[1]
-#print(searchResponse)
+    baseurl = args.url
+    userName = args.username
+    password = args.password
+    searchQuery = args.search
+    
+    # Set up search query
+    searchQuery = searchQuery.strip()
+    # If the query doesn't already start with the 'search' operator or another
+    # generating command (e.g. "| inputcsv"), then prepend "search " to it.
+    if not (searchQuery.startswith('search') or searchQuery.startswith("|")):
+        searchQuery = 'search ' + searchQuery
+    #print(searchQuery)
 
-#sid = '1590206372.1283459'
-sid = None
-if not sid:
-    sid = minidom.parseString(searchResponse).getElementsByTagName('sid')[0].childNodes[0].nodeValue
-print(sid)
+    # Authenticate with server.
+    # Disable SSL cert validation. Splunk certs are self-signed.
+    serverContent = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/auth/login',
+        'POST', headers={}, body=urllib.parse.urlencode({'username':userName, 'password':password}))[1]
+    #print(serverContent)
+    sessionKey = minidom.parseString(serverContent).getElementsByTagName('sessionKey')[0].childNodes[0].nodeValue
+    #print(sessionKey)
 
-#searchStatus = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/search/jobs/' + sid,'GET',
-#    headers={'Authorization': 'Splunk {}'.format(sessionKey)},body=urllib.parse.urlencode({}))[1]
-#print(searchStatus)
+    # Run the search and get the search ID.
+    # Disable SSL cert validation. Splunk certs are self-signed.
+    searchResponse = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/search/jobs','POST',
+        headers={'Authorization': 'Splunk {}'.format(sessionKey)}, body=urllib.parse.urlencode({'search': searchQuery}))[1]
+    #print(searchResponse)
 
-searchResults = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/search/jobs/' + sid + '/events','GET',
-    headers={'Authorization': 'Splunk {}'.format(sessionKey)},body=urllib.parse.urlencode({'output_mode': 'json'}))[1]
+    sid = None
+    #sid = ''
+    if not sid:
+        sid = minidom.parseString(searchResponse).getElementsByTagName('sid')[0].childNodes[0].nodeValue
+    #print(sid)
 
-if searchResults != b'{"preview":true,"init_offset":0,"post_process_count":0,"messages":[],"results":[]}':
-    print(searchResults)
+    # While loop until Splunk search has completed
+    isDone = False
+    while not isDone:
+        searchStatus = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/search/jobs/' + sid,'GET',
+            headers={'Authorization': 'Splunk {}'.format(sessionKey)},body=urllib.parse.urlencode({'output_mode': 'json'}))[1]
+        searchStatus_json = json.loads(searchStatus)
+        isDone = searchStatus_json['entry'][0]['content']['isDone']
+        print("waiting for search to complete...")
+        time.sleep(1)
+        
+    # Pull Search results
+    searchResults = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/search/jobs/' + sid + '/events','GET',
+        headers={'Authorization': 'Splunk {}'.format(sessionKey)},body=urllib.parse.urlencode({'output_mode': 'json'}))[1]
+
+    # If search results contains events, dump to file
+    if searchResults != b'{"preview":true,"init_offset":0,"post_process_count":0,"messages":[],"results":[]}':
+        searchResults_json = json.loads(searchResults)
+        with open('splunk_' + nowFileName + '.json', 'w') as outfile:
+            for result in searchResults_json['results']:
+                json.dump(result, outfile)
+                outfile.write("\n")
+
+if __name__ == '__main__':
+    main()
 
 # References
 # https://docs.splunk.com/Documentation/Splunk/8.0.3/RESTREF/RESTsearch
