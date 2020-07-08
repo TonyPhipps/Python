961b8b0ab7d06c05b49f5d0a28cce158448da70d
Tony Phipps <tphipps@ciphertechs.com>
2020-07-08 16:23:24 -0600

    updated to eliminate deprecated pandas commands

--- get_mitre_attack_techniques.py
+++ get_mitre_attack_techniques.py
@@ -1,7 +1,7 @@
 # https://attackcti.readthedocs.io/en/latest/attackcti_overview.html
 
 from attackcti import attack_client
-from pandas.io.json import json_normalize
+import pandas
 
 outfile = 'C:\\mitre.csv'
 
@@ -9,7 +9,7 @@ client = attack_client()
 
 data = client.get_enterprise(stix_format=False)
 
-data_panda = (json_normalize(data['techniques'])
+data_panda = pandas.json_normalize(data['techniques'])
 
 data_panda[['technique_id','technique','tactic','url']].to_csv(outfile, index=False)
 
