fe0499ba7f3e0bf2e56969f4989c0bff16c8e21e
Tony Phipps <anthony.james.phipps@gmail.com>
2021-07-30 12:59:11 -0600

    updates

--- devo_api.py
+++ devo_api.py
@@ -11,7 +11,8 @@ api_key = ''
 api_secret = ''
 domain = ''
 
-api_url_base = 'https://api-us.devo.com/probio/domain/{}'.format(domain)
+provision_api_url_base = 'https://api-us.devo.com/probio/domain/{}'.format(domain)
+query_api_url_base = 'https://apiv2-us.devo.com/search'
 timestamp = str(int(time.time()) * 1000)
 
 sign = hmac.new(api_secret.encode(), (api_key + timestamp).encode(), hashlib.sha256).hexdigest()
@@ -24,7 +25,7 @@ headers = {
     }
 
 def get_roles():
-    api_url = '{}/roles'.format(api_url_base)
+    api_url = '{}/roles'.format(provision_api_url_base)
     response = requests.get(api_url, headers=headers)
     
     if response.status_code == 200:
@@ -37,7 +38,7 @@ def get_roles():
         return None
 
 def get_role(role, full='false'):
-    api_url = '{}/roles/{}?full={}'.format(api_url_base, role, full)
+    api_url = '{}/roles/{}?full={}'.format(provision_api_url_base, role, full)
     response = requests.get(api_url, headers=headers)
     
     if response.status_code == 200:
@@ -48,5 +49,29 @@ def get_role(role, full='false'):
         print('[!] HTTP {0} calling [{1}]'.format(response.status_code, api_url))
         return None
 
+def post_query():
+    api_url = '{}/query'.format(query_api_url_base)
 
-get_role('Monitor', full='true')
+    data = {
+        "query": "from siem.logtrust.alert.info where priority=5",
+        "from": '60d',
+        "to": 'now',
+        "mode": {
+            "type": "csv"
+        },
+    }
+
+    response = requests.post(api_url, headers=headers, json=data)
+    
+    if response.status_code == 200:
+        json_data = json.loads(response.text)
+        print(json_data)
+
+    else:
+        print('[!] HTTP {0} calling [{1}]'.format(response.status_code, api_url))
+        return None
+
+
+#get_roles()
+#get_role('Administrator', full='true')
+post_query()
